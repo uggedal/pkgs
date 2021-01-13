445eca1e1cef869253ae8ca64deba615552673d2
Eivind Uggedal <eivind@uggedal.com>
2021-01-13 19:07:12 +0000

    cfg-base: initial

---
+++ cfg-base/profile.sh
@@ -0,0 +1,10 @@
+# Sane man page width:
+MANWIDTH=80
+
+# Sane less with: smart case search, quit if less than one screen, skip
+# clearing screen, and display colors:
+LESS=-iFXR
+PAGER=less
+
+# Use custom readline(3) inputrc:
+INPUTRC=/etc/input.local.rc
