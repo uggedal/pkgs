271f3cf99bef5c02bba4955b87008dd32d3aaa92
Eivind Uggedal <eivind@uggedal.com>
2021-01-13 19:46:43 +0000

    cfg-base: fix profile export

--- cfg-base/profile.sh
+++ cfg-base/profile.sh
@@ -1,10 +1,10 @@
 # Sane man page width:
-MANWIDTH=80
+export MANWIDTH=80
 
 # Sane less with: smart case search, quit if less than one screen, skip
 # clearing screen, and display colors:
-LESS=-iFXR
-PAGER=less
+export LESS=-iFXR
+export PAGER=less
 
 # Use custom readline(3) inputrc:
-INPUTRC=/etc/input.local.rc
+export INPUTRC=/etc/input.local.rc
