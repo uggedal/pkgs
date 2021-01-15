0ffe6dacdabde18c2fc196fbb7d7c74e1d78340d
Eivind Uggedal <eivind@uggedal.com>
2021-01-15 10:15:37 +0000

    vim-lastplace: import 3.2.1-1 from aur

--- aur.sh
+++ aur.sh
@@ -4,9 +4,11 @@ ROOT=$(cd "$(dirname "$0")"; pwd -P)
 AUR_URL='https://aur.archlinux.org/cgit/aur.git/snapshot'
 AUR_PACKAGES='
 	needrestart
+	vim-lastplace
 '
 
 for p in $AUR_PACKAGES; do
 	curl $AUR_URL/$p.tar.gz | tar -C $ROOT -xz
 	rm -f $ROOT/$p/.SRCINFO
+	rm -f $ROOT/$p/.gitignore
 done
