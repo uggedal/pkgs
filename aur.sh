cf46c1c261742c868a878ebad78d0f828a2868e9
Eivind Uggedal <eivind@uggedal.com>
2021-01-25 19:07:32 +0000

    python-tasklib: import 2.3.0-1 from aur

--- aur.sh
+++ aur.sh
@@ -4,6 +4,7 @@ ROOT=$(cd "$(dirname "$0")"; pwd -P)
 AUR_URL='https://aur.archlinux.org/cgit/aur.git/snapshot'
 AUR_PACKAGES='
 	needrestart
+	python-tasklib
 	vim-lastplace
 	vim-selenized-git
 	vim-vimwiki
