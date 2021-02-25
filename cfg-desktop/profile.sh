bf9cef9d664b01c1e34b33b87d9cf1b304b074c1
Eivind Uggedal <eivind@uggedal.com>
2021-02-25 10:13:09 +0000

    cfg-desktop: setting DISPLAY in environment breaks sway
    When disabling xwayland sway won't start with DISPLAY in environment

--- cfg-desktop/profile.sh
+++ cfg-desktop/profile.sh
@@ -3,6 +3,3 @@ export MOZ_ENABLE_WAYLAND=1
 
 # Set desktop for xdg-desktop-portal-wlr:
 export XDG_CURRENT_DESKTOP=sway
-
-# Workaround twisted bug https://twistedmatrix.com/trac/ticket/9904
-export DISPLAY=awaiting_twisted_21_2
