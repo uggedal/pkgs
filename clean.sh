0d5819097ce720c3ebe9aeecf54bdfee66728ae0
Eivind Uggedal <eivind@uggedal.com>
2021-03-31 10:12:46 +0000

    clean.sh: rm logfiles

--- clean.sh
+++ clean.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ROOT=$(cd "$(dirname "$0")"; pwd -P)
+
 REPO=/var/local/repo
 REPODB=/var/local/repo/custom.db.tar.gz
 
@@ -13,4 +15,10 @@ dbfiles() {
 
 for f in $(comm -23 <(repofiles) <(dbfiles)); do
 	sudo rm -v $REPO/$f
+done
+
+for f in $ROOT/*/*.log $ROOT/../priv-pkgs/*/*.log; do
+	if [ -e "$f" ]; then
+		rm -v "$f"
+	fi
 done
