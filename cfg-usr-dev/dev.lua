70754b4c2a1863e183dcf7bb0c12b930360216cb
Eivind Uggedal <eivind@uggedal.com>
2021-05-03 09:42:56 +0000

    cfg-usr-dev: autoformat python on save

--- cfg-usr-dev/dev.lua
+++ cfg-usr-dev/dev.lua
@@ -1,1 +1,8 @@
 require'lspconfig'.pyls.setup{}
+
+vim.cmd([[
+	augroup LspFormat
+		autocmd!
+		autocmd FileType python autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
+	augroup END
+]])
