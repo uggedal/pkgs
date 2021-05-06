3cfc99e8b3ed7542a76ffca2ed23c3d5199a57b6
Eivind Uggedal <eivind@uggedal.com>
2021-05-06 09:32:17 +0000

    cfg-usr-dev: telescope lsp mappings

--- cfg-usr-dev/dev.lua
+++ cfg-usr-dev/dev.lua
@@ -1,3 +1,5 @@
+local util = require('util')
+
 require'lspconfig'.pyls.setup{}
 
 vim.cmd([[
@@ -6,3 +8,6 @@ vim.cmd([[
 		autocmd FileType python autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
 	augroup END
 ]])
+
+util.telmap('lr', 'lsp_references')
+util.telmap('ld', 'lsp_definitions')
