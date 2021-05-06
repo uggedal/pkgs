d46e7006f82dc8d04d1697140571faa22ca042f9
Eivind Uggedal <eivind@uggedal.com>
2021-05-06 12:37:45 +0000

    cfg-usr-dev: lua-format

--- cfg-usr-dev/dev.lua
+++ cfg-usr-dev/dev.lua
@@ -1,42 +1,36 @@
 local lspconf = require('lspconfig')
 
 local on_attach = function(client, bufnr)
-	local function bufmap(keys, cmd)
-		local opts = {noremap = true, silent=true}
-		vim.api.nvim_buf_set_keymap(bufnr, 'n', keys, cmd, opts)
-	end
+    local function bufmap(keys, cmd)
+        local opts = {noremap = true, silent = true}
+        vim.api.nvim_buf_set_keymap(bufnr, 'n', keys, cmd, opts)
+    end
 
-	local function buftelmap(keys, cmd)
-		bufmap(
-			keys,
-			"<cmd>lua require('telescope.builtin')." .. cmd .. '()<cr>'
-		)
-	end
+    local function buftelmap(keys, cmd)
+        bufmap(keys, "<cmd>lua require('telescope.builtin')." .. cmd .. '()<cr>')
+    end
 
-	local function buflspmap(keys, cmd)
-		bufmap(
-			keys,
-			"<cmd>lua vim.lsp." .. cmd .. '()<cr>'
-		)
-	end
+    local function buflspmap(keys, cmd)
+        bufmap(keys, "<cmd>lua vim.lsp." .. cmd .. '()<cr>')
+    end
 
-	buftelmap('gr', 'lsp_references')
-	buftelmap('gd', 'lsp_definitions')
-	buftelmap('gd', 'lsp_definitions')
-	buflspmap('gD', 'buf.declaration')
-	buflspmap('gi', 'buf.implementation')
-	buflspmap('K', 'buf.hover')
-	buflspmap('<leader>rn', 'buf.rename')
-	buflspmap('<leader>ca', 'buf.code_action')
+    buftelmap('gr', 'lsp_references')
+    buftelmap('gd', 'lsp_definitions')
+    buftelmap('gd', 'lsp_definitions')
+    buflspmap('gD', 'buf.declaration')
+    buflspmap('gi', 'buf.implementation')
+    buflspmap('K', 'buf.hover')
+    buflspmap('<leader>rn', 'buf.rename')
+    buflspmap('<leader>ca', 'buf.code_action')
 
-	if client.resolved_capabilities.document_formatting then
-		vim.cmd([[
+    if client.resolved_capabilities.document_formatting then
+        vim.cmd([[
 			augroup LspFormat
 				autocmd! * <buffer>
 				autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync(nil, 1000)
 			augroup END
 		]])
-	end
+    end
 end
 
-lspconf.pyls.setup{ on_attach = on_attach }
+lspconf.pyls.setup {on_attach = on_attach}
